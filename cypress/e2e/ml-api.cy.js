b22c3bb62a6b81f4172a50e215d17fbed6330c4c
Germán Buchmüller <german.buchmuller@ing.austral.edu.ar>
2022-06-20 14:32:44 -0300

    Update ml-api.cy.js

--- cypress/e2e/ml-api.cy.js
+++ cypress/e2e/ml-api.cy.js
@@ -12,8 +12,10 @@ describe('ml spec', () => {
     cy.wait(3000)
     cy.get('.nav-search-btn').click({ force: true })
     cy.wait(1000)
-    cy.get('.ui-search-layout__item').should('have.length.at.least', 1).first().click()
-    cy.wait(1000)
+    cy.get('[id=shipping_time_highlighted]').click();
+    cy.wait(3000)
+    cy.get('.ui-search-layout__item').should('have.length.at.least', 1).first().children().children().children('.ui-search-result__image').children('.ui-search-link').click();
+    cy.wait(2000)
     cy.get('.andes-button--quiet').contains("Agregar al carrito").click()
   })
 })
